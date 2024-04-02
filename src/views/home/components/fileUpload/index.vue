b85868e0fc55a7bdf91486abb4cd5b70d6b88222
zhangfan04 <zhangfan04@megvii.com>
2024-04-02 18:45:40 +0800

    feat: 上传文件组件

---
+++ src/views/home/components/fileUpload/index.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="file-upload-wrapper">
+        <input ref="fileInput" v-show="false" type="file" @change="handleFileUpload" />
+        <meg-button class="upload-btn" type="secondary" icon="megui-add" @click="chooseFile">选择文件</meg-button>
+        <div class="file-info" v-show="showFileInfo">
+            <div class="name" :title="fileInfo.name">
+                {{ fileInfo.name }}
+            </div>
+            <span class="view-btn" @click="handleDownLoadFile">查看</span>
+            <img class="close-btn" src="@/assets/close.png" alt="" @click="handleDeleteFile" />
+            <a :href="fileUrl" ref="downloadRef" v-show="false"></a>
+        </div>
+        <div class="progress" v-show="showProgress">{{ progress }}%</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            file: null,
+            fileInfo: {
+                name: null,
+                size: null
+            },
+            fileUrl: null,
+            progress: null,
+            action: 'https://megdesign-static-dev.mcd.megvii-inc.com/v1/common/upload' //TODO: 记得换
+        };
+    },
+    computed: {
+        showFileInfo() {
+            return this.fileInfo.name;
+        },
+        showProgress() {
+            return this.progress && this.progress < 100;
+        }
+    },
+    methods: {
+        chooseFile() {
+            this.$refs.fileInput.click();
+        },
+        handleFileUpload(event) {
+            this.file = event.target.files[0];
+            this.fileInfo = {
+                name: this.file.name,
+                size: (this.file.size / (1024 * 1024)).toFixed(2)
+            };
+            this.uploadFile();
+        },
+        uploadFile() {
+            let formData = new FormData();
+            formData.append('file', this.file);
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', this.action, true);
+
+            xhr.upload.onprogress = event => {
+                if (event.lengthComputable) {
+                    this.progress = Math.round((event.loaded / event.total) * 100);
+                }
+            };
+
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    let responseData = JSON.parse(xhr.responseText);
+                    this.fileUrl = responseData.data.url || null;
+                    console.log('File uploaded successfully:', responseData);
+                } else {
+                    console.error('File upload failed');
+                }
+            };
+
+            xhr.send(formData);
+        },
+        handleDownLoadFile() {
+            this.$refs.downloadRef.dispatchEvent(new MouseEvent('click'));
+        },
+        handleDeleteFile() {
+            this.fileInfo = {
+                name: null,
+                size: null
+            };
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
     <div class="home-wrapper">
         <div class="home-header">
             <div class="left">
-                <span class="title">明厨亮灶行业大模型</span>
+                <span class="title">视联网监管大模型</span>
                 <span class="info">芯片：昇腾910B I 模型参数量：百亿</span>
             </div>
             <div class="right">
@@ -11,7 +11,7 @@
         </div>
         <div class="home-body">
             <div class="left">
-                <div class="title">我的对话</div>
+                <div class="title">监管条例细则展示</div>
                 <meg-input placeholder="搜索历史记录">
                     <img slot="suffix" class="search-icon" src="@/assets/search.svg" alt="" />
                 </meg-input>
@@ -22,14 +22,29 @@
             <div class="right">
                 <div class="upload-content">
                     <div class="title">选择监管文件</div>
-                    <div></div>
+                    <FileUpload />
                 </div>
                 <div class="chat-content">222</div>
             </div>
         </div>
     </div>
 </template>
-<script></script>
+
+<script>
+import FileUpload from './components/fileUpload';
+export default {
+    components: {
+        FileUpload
+    },
+    data() {
+        return {
+            file: null,
+            progress: null
+        };
+    },
+    methods: {}
+};
+</script>
 
 <style lang="scss" scoped>
 @import './index.scss';
